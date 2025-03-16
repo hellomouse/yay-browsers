3269283d3b34baf921dbf2e8ae37996d50b6a313
iczero <iczero4@gmail.com>
2025-03-15 22:52:24 -0700

    poll shorter because puppeteer command timeout

--- lib/event-bridge.ts
+++ lib/event-bridge.ts
@@ -7,38 +7,63 @@ export type EvalTarget = Page | WebWorker;
 
 export interface RemoteEventBridge<T> {
   post: (event: T) => void;
-  poll: () => Promise<T[]>;
+  poll: (timeout?: number) => Promise<T[] | null>;
 }
 
 function remoteCreateBridge<T>() {
   class RemoteEventBridgeImpl<U> {
     events: U[] = [];
     _waiter: (() => void) | null = null;
+    _deferredNotify = false;
 
     post(event: U) {
       this.events.push(event);
-      if (this._waiter) this._waiter();
+      if (this._waiter && !this._deferredNotify) {
+        this._deferredNotify = true;
+        queueMicrotask(() => {
+          this._deferredNotify = false;
+          if (this._waiter && this.events.length > 0) {
+            this._waiter();
+          }
+        });
+      }
     }
 
-    poll(): Promise<U[]> {
+    poll(timeout?: number): Promise<U[] | null> {
       return new Promise(resolve => {
         if (this.events.length) {
           let ev = this.events;
           this.events = [];
           resolve(ev);
         } else {
+          // note: strictly speaking, this type is wrong because this function runs in the browser, but whatever
+          let timeoutId: NodeJS.Timeout;
           if (this._waiter) {
             throw new Error('cannot handle multiple waiters');
           }
-          this._waiter = () => {
+          let waiter = () => {
             if (!this.events.length) {
               throw new Error('waiter resumed with no events');
             }
+            clearTimeout(timeoutId);
             let ev = this.events;
             this.events = [];
             this._waiter = null;
             resolve(ev);
           };
+          this._waiter = waiter;
+          timeoutId = setTimeout(() => {
+            if (Object.is(this._waiter, waiter)) {
+              this._waiter = null;
+              if (this.events.length) {
+                let ev = this.events;
+                this.events = [];
+                resolve(ev);
+              } else {
+                resolve(null);
+              }
+            }
+          }, timeout);
         }
       });
     }
@@ -49,55 +74,66 @@ function remoteCreateBridge<T>() {
 
 export interface EventBridgeEvents<T> {
   event: [T];
-  disposed: [];
+  dispose: [];
 }
 
 export class EventBridge<T> extends EventEmitter<EventBridgeEvents<T>> implements AsyncDisposable {
   _cancel: () => void;
+  _worker: Promise<void>;
+  active = true;
 
-  constructor(public handle: JSHandle<RemoteEventBridge<T>>) {
+  constructor(
+    public handle: JSHandle<RemoteEventBridge<T>>,
+    public readonly pollTimeout = 120_000
+  ) {
     super();
-
     let { promise: cancelSignal, resolve: cancelFn } = Promise.withResolvers<void>();
     this._cancel = cancelFn;
-    (async () => {
+    this._worker = (async () => {
       let cancelled: Promise<typeof CANCELLED> = cancelSignal.then(() => CANCELLED);
-      let pollBridge = () => this.handle.evaluate(remote => remote.poll());
-      loop: while (true) {
+      let pollBridge = () =>
+        this.handle.evaluate((remote, timeout) => remote.poll(timeout), this.pollTimeout);
+      let needsDispose = true;
+      while (true) {
         let result;
         try {
-          result = await Promise.race([
-            cancelled,
-            pollBridge(),
-          ]);
+          result = await Promise.race([cancelled, pollBridge()]);
         } catch (err) {
-          console.warn('error from remote bridge:', err);
-          break loop;
+          if (err instanceof Error && err?.name === 'TargetCloseError') {
+            needsDispose = false;
+          } else {
+            console.warn('error from remote bridge:', err);
+          }
+          break;
         }
 
-        if (result === CANCELLED) {
-          break loop;
-        }
+        if (result === CANCELLED) break;
+        if (result === null) continue;
         for (let event of result) {
           this.emit('event', event);
         }
       }
-      try {
-        await this.handle.dispose();
-      } catch (err) {
-        // don't care
+      if (needsDispose) {
+        try {
+          await this.handle.dispose();
+        } catch (err) {
+          // don't care
+        }
       }
-      this.emit('disposed');
-    })();
+    })().finally(() => {
+      this.active = false;
+      this.emit('dispose');
+    });
   }
 
-  static async new<T>(target: EvalTarget): Promise<EventBridge<T>> {
+  static async new<T>(target: EvalTarget, timeout?: number): Promise<EventBridge<T>> {
     let handle = await target.evaluateHandle(remoteCreateBridge<T>);
-    return new EventBridge<T>(handle);
+    return new EventBridge<T>(handle, timeout);
   }
 
   async dispose() {
-    return this._cancel();
+    this._cancel();
+    await this._worker;
   }
 
   [Symbol.asyncDispose](): Promise<void> {
